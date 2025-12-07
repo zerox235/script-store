b490cfbf348eaec9376d5b8cfa306d21ec00a52a
Zerox <zerox@undefined>
2025-12-07 20:24:03 +0800

    0000000000

---
+++ _func/test.py
@@ -0,0 +1,8 @@
+
+
+_log_info("这是测试Python文件！")
+_log_warn("这是测试Python文件！")
+_log_error("这是测试Python文件！")
+_log_usage("这是测试Python文件！")
+
+
---
+++ _func/test.sh
@@ -0,0 +1,8 @@
+
+
+log_info "这是测试Shell文件！"
+log_warn "这是测试Shell文件！"
+log_error "这是测试Shell文件！"
+log_usage "这是测试Shell文件！"
+
+
